711795b4e099325dcdc0bbb02e39428043152207
rronak <raimaji120@gmail.com>
2026-01-05 16:29:14 -0400

    Updated for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <-- this enables static export
+  output: 'export',        // enables static HTML export
+  trailingSlash: true,     // helps GitHub Pages serve files correctly
+  reactStrictMode: true,   // optional
 };
 
 module.exports = nextConfig;
